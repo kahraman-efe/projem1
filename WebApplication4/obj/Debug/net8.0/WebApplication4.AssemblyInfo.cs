2f589742db5588d42c59890ef82525a2634bc766
kahramanefe.dev <erden530@gmail.com>
2026-09-08 19:54:05 +0300

    skiller bölümüne ikon paketi ve ikonlar eklendi

--- WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e67ee6c1b1da1ced01ed69780261380ae310595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00325b3d083bc25b0154f0eab4bd2d82957830d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
